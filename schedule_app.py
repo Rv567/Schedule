deea9bbdeac37b05bc7a4bc3d086728da4448669
Rv567 <103488188+Rv567@users.noreply.github.com>
2024-07-05 11:53:54 +0100

    Update schedule_app.py

--- schedule_app.py
+++ schedule_app.py
@@ -2,12 +2,18 @@ import streamlit as st
 import schedule
 import time
 from datetime import datetime
-import threading
+#import threading
+
+# Streamlit app code
+st.title("My Streamlit App with Scheduler")
+st.write("The function will run every day at 08:00 AM")
 
 # Your function to be scheduled
 def my_scheduled_function():
-    st.write(f"Function is running: {datetime.now()}")
+    print(f"Function is running: {datetime.now()}")
     # Add your function logic here
+
+schedule.every(5).seconds.do(my_scheduled_function)
 
 # Function to run the scheduler
 def run_scheduler():
@@ -15,13 +21,4 @@ def run_scheduler():
         schedule.run_pending()
         time.sleep(1)
 
-# Schedule your function to run every day at a specific time
-schedule.every(5).seconds.do(my_scheduled_function)
-
-# Start the scheduler in a separate thread
-scheduler_thread = threading.Thread(target=run_scheduler, daemon=True)
-scheduler_thread.start()
-
-# Streamlit app code
-st.title("My Streamlit App with Scheduler")
-st.write("The function will run every day at 08:00 AM")
+run_scheduler()
